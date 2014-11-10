206ec1789d0ea846c44d46b46c0acbad26f6a558
Fonso_s <alfonsosandom@gmail.com>
2014-11-10 11:14:24 +0100

    Added Back to top button

--- js/smoth-scroll.js
+++ js/smoth-scroll.js
@@ -19,7 +19,28 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 });
 
 
-$(".navbar-brand[href^='#']").on('click', function(e) {
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -300
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
    e.preventDefault();
